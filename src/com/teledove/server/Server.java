640ef508b082d816ccc4db690d7b11f0568c06f0
noneface <nonefacesay@gmail.com>
2017-05-15 22:57:08 +0800

    add userState.java  and connectListener ,server.java

--- src/com/teledove/server/Server.java
+++ src/com/teledove/server/Server.java
@@ -1,9 +1,30 @@
 package com.teledove.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
+import com.teledove.model.User;
+import com.teledove.model.userState;
+
 public class Server {
+	
+	private HashMap<userState, Socket> socketPool;
+	private ServerSocket serverSocket;
+	
 	public Server() {
-		// this is Server 
-		
+		 
+		this.socketPool = new HashMap<>();
+		try {
+			serverSocket = new ServerSocket(9999);  //  listen the port ,and accept the new socket connect
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) {
