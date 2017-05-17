bc01628d28be5eb22dd9338fbf6d2f04e6753f61
liubanxian <miniyyyyu@gmail.com>
2017-05-17 20:37:55 +0800

    update dependices

--- src/com/teledove/dao/HibernateUtil.java
+++ src/com/teledove/dao/HibernateUtil.java
@@ -6,7 +6,7 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
-public class HibernateUtil {
+public class HibernateUtil { 
     private final static SessionFactory sessionFactory=buildSessionFactory();
     private static SessionFactory buildSessionFactory() {
     	Configuration configuration=new Configuration().configure();
