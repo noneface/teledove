4fd9170d2c8a9f3822defc4f3eee5fd88e01c6fd
noneface <nonefacesay@gmail.com>
2017-05-20 13:30:05 +0800

    init startup

--- src/com/teledove/windowComponet/loginFrame.java
+++ src/com/teledove/windowComponet/loginFrame.java
@@ -8,12 +8,8 @@ public class loginFrame extends JFrame{
 	   mainPanel mainpanel = new mainPanel();
 	   this.setContentPane(mainpanel);
 	   this.setBounds(700,290,597,467);
-
 	   this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		this.validate();
-		this.setVisible(true);
-	}
-      public static void main(String[] args) {
-		new loginFrame();
+	   this.validate();
+	   this.setVisible(true);
 	}
 } 
