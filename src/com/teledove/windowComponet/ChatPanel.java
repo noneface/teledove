5b7a0d86fe8f65cfcad31fd9bc2e9bf0b759fd97
liubanxian <miniyyyyu@gmail.com>
2017-05-22 15:34:27 +0800

    aa

--- src/com/teledove/windowComponet/ChatPanel.java
+++ src/com/teledove/windowComponet/ChatPanel.java
@@ -20,6 +20,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.awt.event.ActionEvent;
 import java.awt.Component;
+import java.awt.Font;
+
 import javax.swing.Box;
 
 
@@ -29,6 +31,9 @@ public class ChatPanel extends JPanel{
 	public String username;
 	private JTextPane textPane;
 	private JEditorPane editorPane;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JComboBox comboBox_1;
 	
 	public ChatPanel(String username, Client client){
 		this.client = client;
@@ -79,13 +84,43 @@ public class ChatPanel extends JPanel{
 	  button_1.setBounds(424, 455, 113, 27);
 	  add(button_1);
 	  
+	  
+	  lblNewLabel = new JLabel("\u5B57\u4F53\u5927\u5C0F");
+	  lblNewLabel.setBounds(177, 334, 72, 18);
+	  add(lblNewLabel);
+	  
+	  JComboBox comboBox = new JComboBox();
+	  comboBox.addItem("15");
+	  comboBox.addItem("25");
+	  comboBox.addActionListener(new ActionListener() {
+		  	public void actionPerformed(ActionEvent arg0) {
+		  	  int num =Integer.parseInt(comboBox.getSelectedItem().toString());
+			  editorPane.setFont(new Font(comboBox_1.getSelectedItem().toString(),Font.PLAIN,num));
+		  	}
+		  });
+	  comboBox.setBounds(263, 331, 72, 24);
+	  add(comboBox);
+	  
+	  lblNewLabel_1 = new JLabel("\u5B57\u4F53");
+	  lblNewLabel_1.setBounds(24, 334, 72, 18);
+	  add(lblNewLabel_1);
+	  
+	  comboBox_1 = new JComboBox();
+	  comboBox_1.addItem("宋体");
+	  comboBox_1.addItem("隶书");
+	  comboBox_1.addItem("黑体");
+	  comboBox_1.setBounds(69, 331, 67, 24);
+	  add(comboBox_1);
+	  
+	  
 	  this.editorPane = new JEditorPane();
 	  editorPane.setBounds(14, 368, 523, 78);
 	  add(editorPane);
-	  
-	  JLabel lblTeledove = new JLabel("\u2014\u2014> \u2014\u2014> \u2014\u2014> \u2014\u2014> \u2014\u2014> \u2014\u2014> \u2014\u2014> \u2014\u2014> \u2014\u2014>     Teledove");
+
+	  /*JLabel lblTeledove = new JLabel("\u2014\u2014> \u2014\u2014> \u2014\u2014> \u2014\u2014> \u2014\u2014> \u2014\u2014> \u2014\u2014> \u2014\u2014> \u2014\u2014>     Teledove");
 	  lblTeledove.setBounds(14, 337, 523, 18);
-	  add(lblTeledove);
+	  add(lblTeledove);*/
+	  
   	  
   	  this.validate();
      }
